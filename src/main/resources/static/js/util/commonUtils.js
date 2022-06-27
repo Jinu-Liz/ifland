4703b4998532168122eee64b5bc5913ff1cd3599
Hyungseock-Yun <bagzein@gmail.com>
2022-06-28 03:38:41 +0900

    - 기능 수정

--- src/main/resources/static/js/util/commonUtils.js
+++ src/main/resources/static/js/util/commonUtils.js
@@ -1,3 +1,9 @@
-String.prototype.isEmpty = function (str) {
-  return str === null || str === '' || str === undefined || str === 'null';
+String.prototype.isEmpty = function () {
+  let str = this.toString();
+  return str.length === 0 || str === '' || str === undefined || str === null || str === 'null';
+}
+
+String.prototype.isNotEmpty = function () {
+  let str = this.toString();
+  return str.length > 0 || str !== '' || str !== undefined || str !== 'null';
 }
