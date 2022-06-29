d01956c644c62cf3d3c2dbf57119c53ff4dbde60
Hyungseock-Yun <bagzein@gmail.com>
2022-06-30 03:11:00 +0900

    - 댓글 비어있을 시 버튼 비활성화 - 댓글 300자 제한, 넘을 시 알람 및 초과 내용 삭제

--- src/main/resources/static/js/profile/detail.js
+++ src/main/resources/static/js/profile/detail.js
@@ -1,12 +1,29 @@
 $(document).ready(function () {
-  $('#comment').on('keyup', function () {
+  const comment = $('#comment');
+  const commentBtn = $("#comment-button");
+
+  comment.on('keyup', function () {
+
+    if ($(this).val().length > 0) buttonActive(commentBtn);
+
+    if ($(this).val().length <= 0) buttonDisabled(commentBtn);
+
     if ($(this).val().length > 300) {
       alert("300자를 초과하였습니다.");
       $(this).val($(this).val().substring(0, 300));
     }
   })
 
-  $('#comment-button').click(function () {
-    if ($(this).val().isEmpty()) alert("내용을 작성해 주세요.");
-  })
+  if (comment.val().isEmpty()) buttonDisabled(commentBtn);
+
 })
+
+const buttonActive = function (commentBtn) {
+  commentBtn.css("background", "#e53637");
+  commentBtn.attr('disabled', false);
+}
+
+const buttonDisabled = function (commentBtn) {
+  commentBtn.attr('disabled', true);
+  commentBtn.css("background", "#dee2e6");
+}
