e5a9e6c233e6ad7350a0f21d9010f54bc8cea1f1
Hyungseock-Yun <bagzein@gmail.com>
2023-01-17 02:47:21 +0900

    - 알림 공통화 - 비밀번호 찾기 알림 리팩터링

--- src/main/resources/static/js/util/commonUtils.js
+++ src/main/resources/static/js/util/commonUtils.js
@@ -1,9 +1,50 @@
-String.prototype.isEmpty = function () {
-  let str = this.toString();
-  return str.length === 0 || str === '' || str === undefined || str === null || str === 'null';
+isEmpty = function (str) {
+  return str === undefined || str === null || str === 'null' || str === '' || str.length === 0;
 }
 
-String.prototype.isNotEmpty = function () {
-  let str = this.toString();
-  return str.length > 0 || str !== '' || str !== undefined || str !== 'null';
+isNotEmpty = function (str) {
+  return str !== undefined || str !== null || str !== 'null' || str !== '' || str.length > 0;
+}
+
+class MsgType {
+  static SUCCESS = "success"
+  static ERROR = "error"
+  static WARNING = "warning"
+  static INFO = "info"
+  static QUESTION = "question"
+}
+
+class Message {
+  constructor(text, icon) {
+    this.text = text;
+    this.icon = icon
+    this.color = '#ffffff';
+    this.background = '#212529';
+    this.confirmButtonColor = '#e53637';
+  }
+
+  static success = (text) => {
+    let msg = new Message(text, MsgType.SUCCESS);
+    Swal.fire(msg);
+  }
+
+  static error = (text) => {
+    let msg = new Message(text, MsgType.ERROR);
+    Swal.fire(msg);
+  }
+
+  static warning = (text) => {
+    let msg = new Message(text, MsgType.WARNING);
+    Swal.fire(msg);
+  }
+
+  static info = (text) => {
+    let msg = new Message(text, MsgType.INFO);
+    Swal.fire(msg);
+  }
+
+  static question = (text) => {
+    let msg = new Message(text, MsgType.QUESTION);
+    Swal.fire(msg);
+  }
 }
