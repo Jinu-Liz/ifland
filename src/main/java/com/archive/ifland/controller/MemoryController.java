2a1fe03f7a1cca75eea927ce13601930a2c9207e
Hyungseock-Yun <bagzein@gmail.com>
2023-09-18 23:57:20 +0900

    - 추억저장소 시작 > 사진 업로드용

---
+++ src/main/java/com/archive/ifland/controller/MemoryController.java
@@ -0,0 +1,23 @@
+package com.archive.ifland.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/memorize")
+public class MemoryController {
+
+
+  @GetMapping("/content")
+  public String content(Model model) {
+    model.addAttribute("cp", "memorize");
+
+    return "main/save-memory";
+
+  }
+
+}
