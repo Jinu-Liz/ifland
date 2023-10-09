c0a9784fb0d7d4d6bd60e91221f1e6248053c113
hsyoon <bagzein@gmail.com>
2023-10-09 23:51:11 +0900

    - Memory API용 컨트롤러 생성 - 댓글 추가 기능

--- src/main/java/com/archive/ifland/controller/api/MemoryApiController.java
+++ src/main/java/com/archive/ifland/controller/api/MemoryApiController.java
@@ -1,10 +1,21 @@
 package com.archive.ifland.controller.api;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/memorize")
 public class MemoryApiController {
 
+  @PostMapping("/comment")
+  public void writeComment(@RequestBody String data) {
+    JsonParser parser = new JsonParser();
+    JsonObject obj = parser.parse(data).getAsJsonObject();
+  }
 }
