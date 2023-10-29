a8f4e876e87bfc012479a24b39e54f83dd6ffc1d
hsyoon <bagzein@gmail.com>
2023-10-29 16:56:27 +0900

    - 테스트용 파일 업로드용 로직

---
+++ src/main/java/com/archive/ifland/dto/Item.java
@@ -0,0 +1,18 @@
+package com.archive.ifland.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Item {
+
+  private Long id;
+
+  private String itemName;
+
+  private UploadFile attachFile;
+
+  private List<UploadFile> imageFiles;
+
+}
---
+++ src/main/java/com/archive/ifland/dto/ItemForm.java
@@ -0,0 +1,19 @@
+package com.archive.ifland.dto;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+public class ItemForm {
+
+  private Long itemId;
+
+  private String itemName;
+
+  private List<MultipartFile> imageFiles;
+
+  private MultipartFile attachFile;
+
+}
